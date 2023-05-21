e902fe660cf4d33face512ae84d422be0fe412f2
nxx5xxx <dnwnx123@naver.com>
2023-05-22 01:09:13 +0900

    good

--- sourcefiles/sql/pro03.sql
+++ sourcefiles/sql/pro03.sql
@@ -85,12 +85,24 @@ create table ACCOM(ANO VARCHAR(5),
 
 -- 회원 더미 데이터
 insert into 	MEMBER	 values('10000','admin','1234','경기도 고양시','010-0000-0000','admin@naver.com',default);
-insert into 	MEMBER	 values('10001','kim1','1111','경기도 고양시','010-1111-1111','test1@naver.com',default);
-insert into 	MEMBER	 values('10002','kim2','2222','경기도 고양시','010-2222-2222','test2@naver.com',default);
-insert into 	MEMBER	 values('10003','kim3','3333','경기도 고양시','010-3333-3333','test3@naver.com',default);
-insert into 	MEMBER	 values('10004','kim4','4444','경기도 고양시','010-1111-1112','test4@naver.com',default);
-insert into 	MEMBER	 values('10005','kim5','5555','경기도 고양시','010-2222-2223','test5@naver.com',default);
-insert into 	MEMBER	 values('10006','kim6','6666','경기도 고양시','010-3333-3334','test6@naver.com',default);
+insert into 	MEMBER	 values('10001','김길동','1111','경기도 고양시','010-1111-1111','test1@naver.com',default);
+insert into 	MEMBER	 values('10002','박길은','2222','경기도 고양시','010-2222-2222','test2@naver.com',default);
+insert into 	MEMBER	 values('10003','이길금','3333','경기도 고양시','010-3333-3333','test3@naver.com',default);
+insert into 	MEMBER	 values('10004','최백금','4444','경기도 고양시','010-1111-1112','test4@naver.com',default);
+insert into 	MEMBER	 values('10005','조백은','5555','경기도 고양시','010-2222-2223','test5@naver.com',default);
+insert into 	MEMBER	 values('10006','김백동','6666','경기도 고양시','010-3333-3334','test6@naver.com',default);
+
+
+-- 기존 더미데이터 삭제하고 한글이름으로 전환
+-- delete from member where mno >= '10001';
+
+-- sql인서트문
+-- insert into MEMBER values(?,?,?,?,?,?,default);
+-- 특정회원 검색문
+-- 이것때문에 id 해야할듯해요
+-- select * from member where 
+
+
 
 -- 공지사항 더미 데이터
 desc notice;
@@ -106,5 +118,19 @@ insert into	notice values('20009','더미제목9','더미본문입니다9','1000
 insert into	notice values('20010','더미제목10','더미본문입니다10','10000','admin','','','',default,default);
 insert into	notice values('20011','더미제목11','더미본문입니다11','10000','admin','','','',default,default);
 insert into	notice values('20012','더미제목12','더미본문입니다12','10000','admin','','','',default,default);
+-- sql 인서트문 
+-- insert into notice values(?,?,?,?,?,?,?,?,default,default);
+desc complain;
 
-select * from notice;
+-- 민원 더미데이터
+insert into complain values('30001', '민원제목1', '민원에대한 본문입니다1','10001', '김길동', '무직','경기도 고양시 일산구','010-1111-1111','첨부파일없음',default,default);
+insert into complain values('30002', '민원제목2', '민원에대한 본문입니다2','10001', '김길동', '무직','경기도 고양시 일산구','010-1111-1111','첨부파일없음',default,default);
+insert into complain values('30003', '민원제목3', '민원에대한 본문입니다3','10001', '김길동', '무직','경기도 고양시 일산구','010-1111-1111','첨부파일없음',default,default);
+insert into complain values('30004', '민원제목4', '민원에대한 본문입니다4','10001', '김길동', '무직','경기도 고양시 일산구','010-1111-1111','첨부파일없음',default,default);
+insert into complain values('30005', '민원제목5', '민원에대한 본문입니다5','10001', '김길동', '무직','경기도 고양시 일산구','010-1111-1111','첨부파일없음',default,default);
+
+desc photog;
+
+-- 포토갤러리 더미데이터
+
+-- insert into photog values()
